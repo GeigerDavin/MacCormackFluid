087c6fadbfd81e10809251578e026c7b30e6e403
Davin Geiger <davin.geiger@googlemail.com>
2016-06-20 17:39:46 +0200

    Implemented Camera Feature
    It is now possible to Change the View with the Mouse and Keyboard.

--- MacCormackFluid/main.cpp
+++ MacCormackFluid/main.cpp
@@ -7,7 +7,7 @@
 #include "OpenGLBuffer.hpp"
 #include "OpenGLShaderProgram.hpp"
 #include "OpenGLVertexArrayObject.hpp"
-
+#include "Camera.hpp"
 
 #define WINDOW_HEIGT (640)
 #define WINDOW_WIDTH (480)
@@ -181,26 +181,10 @@ int main()
 		);
 	bufferVertex.release();
 
-	// Get a handle for our "MVP" uniform
-	GLuint MatrixID = shader.getUniformLocation("MVP");
-
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
-	glm::mat4 View = glm::lookAt(
-		glm::vec3(4, 3, -3), // Camera is at (4,3,-3), in World Space
-		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-		);
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
-
-	// in the "MVP" uniform
-	shader.setUniformValue(MatrixID,MVP);
-
 	vertexes.release();
+
+	// Cull triangles which normal is not towards the camera
+	glEnable(GL_CULL_FACE);
 
 	////GLuint positionsVBO;
 	//struct cudaGraphicsResource* positionsVBO_CUDA;
@@ -213,14 +197,26 @@ int main()
 	//cudaGraphicsGLRegisterBuffer(&positionsVBO_CUDA, positionsVBO, cudaGraphicsMapFlagsWriteDiscard);
 
 	// Loop until the User closes the Main Window
+
+	Camera camera(mainWindow);
 	while (!glfwWindowShouldClose(mainWindow))
 	{
 		// Render here 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
 		shader.bind();
 		{
 			OpenGLVertexArrayObject::Binder binder(&vertexes);
 			// Draw the triangle !
+			camera.computeMatricesFromInputs();
+
+			// Get a handle for our "MVP" uniform
+			GLuint MatrixID = shader.getUniformLocation("MVP");
+
+			glm::mat4 MVP = camera.getProjectionMatrix() * camera.getViewMatrix() * glm::mat4(1.0f);
+
+			// in the "MVP" uniform
+			shader.setUniformValue(MatrixID, MVP);
 			glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices starting at 0 -> 12 triangles
 
 		}
@@ -232,6 +228,11 @@ int main()
 
 		// Poll for and process events
 		glfwPollEvents();
+
+		if (glfwGetKey(mainWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(mainWindow, true);
+		}
 	}
 
 	// Exit GLFW Cleanly
